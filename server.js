4275f4d2685a5366639f9476c9f7c228a9b5f9fa
bape001 <bape001@hotmail.com>
2014-07-20 14:45:12 -0500

    borrar 3 archivos

--- server.js
+++
@@ -1,9 +0,0 @@
-var cv=require("socket.io").listen(6969);
-cv.sockets.on("connection",arranque);
-
-function arranque(usuario){
-	usuario.on("nuevoNombre", emitir);
-}
-function emitir(data){
-	cv.sockets.emit("nombreDesdeServidor", data + "*");
-}
